59e05769a0ad253cab96e6fbea5c6610272ce9b5
Alena Sviridova <svirel2000@mail.ru>
2022-03-01 03:44:27 +0300

    find front

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,27 @@
-import { NestFactory } from '@nestjs/core';
+/*import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const PORT = process.env.PORT || 3000;
+  await app.listen(PORT, () => {
+    console.log(`Our app is running on port ${ PORT }`);
+  });
+}
+bootstrap();*/
+import { NestFactory } from '@nestjs/core';
+import { NestExpressApplication } from '@nestjs/platform-express';
+import { join } from 'path';
+import { AppModule } from './app.module';
+
+async function bootstrap() {
+  const app = await NestFactory.create<NestExpressApplication>(
+      AppModule,
+  );
+
+  app.useStaticAssets(join(__dirname, '..', 'public'));
+  app.setBaseViewsDir(join(__dirname, '..', 'views'));
+  app.setViewEngine('hbs');
   const PORT = process.env.PORT || 3000;
   await app.listen(PORT, () => {
     console.log(`Our app is running on port ${ PORT }`);
